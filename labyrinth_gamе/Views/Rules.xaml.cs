72962a9d1a3eda8799b9cf658a8eb172d8e926ce
Kate Tsaruk <ipz224_tska@student.ztu.edu.ua>
2024-05-12 21:31:31 +0300

    Added game rules

---
+++ labyrinth_gamе/Views/Rules.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace labyrinth_gamе.Views
+{
+    public partial class Rules : Page
+    {
+        public Rules()
+        {
+            InitializeComponent();
+        }
+        private void Image_MouseDown_6(object sender, MouseButtonEventArgs e)
+        {
+            NavigationService.Navigate(new Labyrinth(new UserFactory()));
+        }
+        private void Image_MouseDown_ArrowR(object sender, MouseButtonEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Views/Rules_2.xaml", UriKind.Relative));
+        }
+    }
+}
---
+++ labyrinth_gamе/Views/Rules_2.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace labyrinth_gamе.Views
+{
+    public partial class Rules_2 : Page
+    {
+        public Rules_2()
+        {
+            InitializeComponent();
+        }
+        private void Image_MouseDown_Exit(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void Image_MouseDown_ArrowL(object sender, MouseButtonEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Views/Rules.xaml", UriKind.Relative));
+        }
+        private void Image_MouseDown_6(object sender, MouseButtonEventArgs e)
+        {
+            NavigationService.Navigate(new Labyrinth(new UserFactory()));
+        }
+    }
+}
